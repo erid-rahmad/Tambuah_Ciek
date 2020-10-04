0b522f449ac194b4c5db8881337d03ff5c896d7d
eridr <rahmad.erid@multipolar.com>
2020-10-04 14:45:04 +0700

    jumlah total rincian data 1

--- restaurant/src/main/java/com/tambuahciek/restaurant/Implement/ImplementOrder.java
+++ restaurant/src/main/java/com/tambuahciek/restaurant/Implement/ImplementOrder.java
@@ -1,11 +1,15 @@
 package com.tambuahciek.restaurant.Implement;
 import com.tambuahciek.restaurant.Interface.Order;
+import com.tambuahciek.restaurant.model.ListMakanan;
 import com.tambuahciek.restaurant.model.Makanan;
+import com.tambuahciek.restaurant.model.Pembeli;
+import com.tambuahciek.restaurant.repository.RepositiryPembeli;
 import com.tambuahciek.restaurant.repository.RepositoryMakanan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ImplementOrder implements Order {
@@ -13,20 +17,32 @@ public class ImplementOrder implements Order {
     @Autowired
     RepositoryMakanan repositoryMakanan;
 
+    @Autowired
+    RepositiryPembeli repositiryPembeli;
+    Makanan makanan;
+    Pembeli pembeli;
+
     @Override
     public List<Makanan>  getall(int idpembeli) {
-        Makanan makanan;
+
         List<Makanan> listbyid = repositoryMakanan.listorderbypembeli(idpembeli);
         System.out.println(listbyid);
-//        List<Integer> harga =makanan.getHarga(listbyid);
-//        System.out.println(harga);
-        int x =0;
+
+        //count total tagihan
+        int totaltagihan =0;
         for (Makanan _makanan: listbyid){
 //            x = _makanan.getHarga();
-            System.out.println(x);
-            x+=_makanan.getHarga();
+            System.out.println(totaltagihan);
+            totaltagihan+=_makanan.getHarga();
         }
-        System.out.println(x);
+        System.out.println(totaltagihan);
+
+
+        long id =idpembeli;
+        Pembeli pembeli = repositiryPembeli.getOne(id);
+
+        pembeli.setTagihan(totaltagihan);
+        repositiryPembeli.save(pembeli);
         return listbyid;
     }
 }
